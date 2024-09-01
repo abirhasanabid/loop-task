f5c9c32c5b712e7479a5c4278368b31c19503b4f
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-09-01 22:28:57 +0600

    added task-4

--- task-4.js
+++ task-4.js
@@ -6,3 +6,15 @@ As Ersa is learning now, she wants to explore more and more. Tell Ersa to genera
 
 
 /*programming hero*/
+
+
+let number = 1;
+const five = 5;
+
+while (number <= 10) {
+    if (five == 5) {
+        let multi = five * number;
+        console.log(five, '*', number, '=', multi);
+    }
+    number++;
+}
